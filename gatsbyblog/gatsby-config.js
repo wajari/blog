42f1523da56ad5f6411b5e4fd4668c3d20768c08
wajari <wajari@gmail.com>
2020-03-05 21:10:30 +0100

    Practice with Data and GraphQL

--- gatsbyblog/gatsby-config.js
+++ gatsbyblog/gatsby-config.js
@@ -5,7 +5,18 @@
  */
 
 module.exports = {
+  siteMetadata: {
+    title: `Title from siteMetadata`,
+  },
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    `gatsby-plugin-emotion`,
     {
       resolve: `gatsby-plugin-typography`,
       options: {
--- gatsbyblog/src/pages/about.js
+++ gatsbyblog/src/pages/about.js
@@ -1,13 +1,24 @@
 import React from "react"
+import { graphql } from "gatsby"
 import Header from "./components/header"
 import Layout from "./components/layout"
 
-export default () => (
+export default ({ data }) => (
   <Layout>
     <div style={{ color: `teal` }}>
-      <Header headerText={"About Gatsby"} />
+      <h1>About {data.site.siteMetadata.title}</h1>
       <Header headerText="It's pretty cool" />
       <p>Such wow. Very React.</p>
     </div>
   </Layout>
 )
+
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
